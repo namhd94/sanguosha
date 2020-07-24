5c7854445f5e52144f62d37f8740cedcb8b41cfe
Nam Hoang <namhd94@gmail.com>
2020-07-24 15:59:33 +0700

    create project

---
+++ src/main/java/com/boardgame/sanguosha/SanguoshaApplication.java
@@ -0,0 +1,13 @@
+package com.boardgame.sanguosha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SanguoshaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SanguoshaApplication.class, args);
+    }
+
+}
