0c1b3180bc054ecc51b33c1ae2157ba34f34de61
Nam Dai Hoang <namdh@hitachiconsulting.com>
2020-07-28 19:38:27 +0700

    Add hyperlink

--- dbscript/instert-data.sql
+++ dbscript/instert-data.sql
@@ -19,7 +19,7 @@ VALUES ('Quỹ Tài'
 -- Clarification
 INSERT INTO public.clarification(content, type, card_name)
 VALUES (
-'Kĩ năng Phản Quỹ cho phép Tư mã Ý lấy bài trên tay hoặc bài trong vùng
+'Kĩ năng Phản Quỹ cho phép Tư Mã Ý lấy bài trên tay hoặc bài trong vùng
     trang bị, nhưng không thể lấy bài trong vùng phán xét.'
 , 1
 , 'sima-yi'
@@ -28,8 +28,8 @@ INSERT INTO public.clarification(content, type, card_name)
 VALUES (
 'Kĩ năng Phản Quỹ được tính trên mỗi lượt nhận sát thương. Cho dù Tư Mã Ý
     nhận nhiều sát thương cùng lúc (ví dụ như sát thương từ lá
-    [Đánh] có kèm lá
-    [Rượu]), hắn cũng
+    <a href="/cards/detail/attack">[Đánh]</a> có kèm lá
+    <a href="/cards/detail/wine">[Rượu]</a>), hắn cũng
     chỉ có thể lấy 1 lá từ người gây sát thương.'
 , 1
 , 'sima-yi'
@@ -58,7 +58,7 @@ VALUES (
 'Phản Quỹ của Tư Mã Ý có thể là một công cụ răn đe mạnh mẽ đối với những
     người chơi khác muốn gây sát thương cho hắn. Những người chơi có trang bị
     hữu ích hoặc
-    [Đào] có thể miễn
+    <a href="/cards/detail/peach">[Đào]</a> có thể miễn
     cưỡng không gây tổn hại cho Tư Mã Ý vì lo sợ rằng Tư Mã Ý sẽ kích hoạt Phản
     Quỹ để lấy những thẻ bài này.'
 , 2
@@ -68,22 +68,22 @@ INSERT INTO public.clarification(content, type, card_name)
 VALUES (
 'Quỹ Tài mang lại cho Tư Mã Ý quyền kiểm soát thẻ phán quyết. Ví dụ bạn có
     thể đổi phán xét nếu như đồng minh của bạn là nạn nhân của lá
-    [Binh Lương Thốn Đoạn]'
+    <a href="/cards/detail/capture">[Binh Lương Thốn Đoạn]</a>'
 , 2
 , 'sima-yi'
 );
 INSERT INTO public.clarification(content, type, card_name)
 VALUES (
 'Nếu Tư Mã Ý trang bị
-    [Bát Quái Trận], hắn ta có thể thay đổi phán xét thành màu đỏ để được coi như lá
-    [Thiểm]'
+    <a href="/cards/detail/eight-trigrams">[Bát Quái Trận]</a>, hắn ta có thể thay đổi phán xét thành màu đỏ để được coi như lá
+    <a href="/cards/detail/escape">[Thiểm]</a>'
 , 2
 , 'sima-yi'
 );
 INSERT INTO public.clarification(content, type, card_name)
 VALUES (
 'Tư Mã Ý đặc biệt nguy hiểm khi
-    [Sấm sét]. Nếu
+    <a href="/cards/detail/lightning">[Sấm sét]</a>. Nếu
     trong tay hắn có những lá từ 2 bích đến 9 bích, hắn ta có thể tấn công bất
     kì ai mà hắn ta muốn. Ngoài ra, hắn ta cũng có thể bảo vệ bất kì ai nếu như
     thẻ bài trên tay hắn không thuộc từ 2 bích đến 9 bích.'
@@ -104,7 +104,7 @@ INSERT INTO public.clarification(content, type, card_name)
 VALUES (
 'Như đã nêu trong phần ''Clarification'', một cách để có được sức mạnh của Tư Mã Ý là
 gây ra nhiều hơn một đơn vị sát thương trong một trường hợp cụ thể, chẳng hạn như
-tấn công anh ta bằng [Rượu]. Các chiến lược liên quan đến việc gây ra nhiều hơn
+tấn công anh ta bằng <a href="/cards/detail/wine">[Rượu]</a>. Các chiến lược liên quan đến việc gây ra nhiều hơn
 một đơn vị sát thương trong một lần sẽ làm giảm khả năng tồn tại của "Phản Quỹ"
 và giảm đáng kể cơ hội sống sót lâu dài của anh ta, vì anh ta chỉ có thể lấy một
 thẻ.'
@@ -113,7 +113,7 @@ thẻ.'
 );
 INSERT INTO public.clarification(content, type, card_name)
 VALUES (
-'Mặc dù Tư Mã Ý có thể thay đổi thẻ phán xét thành bất kỳ lá bài nào, anh ta không có khả năng đổi phán xét khác mà [Trương Giác] có. [Trương Giác] có thể lấy thẻ
+'Mặc dù Tư Mã Ý có thể thay đổi thẻ phán xét thành bất kỳ lá bài nào, anh ta không có khả năng đổi phán xét khác mà <a href="/cards/detail/zhang-jiao">[Trương Giác]</a> có. <a href="/cards/detail/zhang-jiao">[Trương Giác]</a> có thể lấy thẻ
 phán xét ban đầu. Tư Mã Ý luôn bị mất
 thẻ bài khi anh kích hoạt "Quỷ Tài".'
 , 3
@@ -123,7 +123,7 @@ INSERT INTO public.clarification(content, type, card_name)
 VALUES (
 'Như đã nêu trước đây, thứ tự lần lượt xác định
 ai là người có tiếng nói cuối cùng trong phán đoán. Đây không phải là điểm yếu
-của mỗi người, nhưng nếu Sima Yi có ít người ở bên phải hơn [Trương Giác], anh ta
+của mỗi người, nhưng nếu Sima Yi có ít người ở bên phải hơn <a href="/cards/detail/zhang-jiao">[Trương Giác]</a>, anh ta
 có thể gặp bất lợi nếu hai người không ở cùng một đội.'
 , 3
 , 'sima-yi'
